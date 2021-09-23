18d016c45a7d50238c065e2c3c9337cb4bc32cd5
Jorge Luis Boeira Bavaresco <jlbavaresco@gmail.com>
2021-09-23 10:17:23 -0300

    adicionando cors origin *

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use(cors())
+app.use(cors({
+    origin: '*'
+}))
 
 const controleEditora = require('./controladores/editoras')
 const controleLivro = require('./controladores/livros')
